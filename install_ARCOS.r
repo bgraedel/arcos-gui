512cae142ea01ccc32479a88abe27398e7fa3fa8
Benjamin Graedel <benjamin.graedel@unibe.ch>
2022-02-21 20:31:09 +0100

    tox ran on windows

---
+++ install_ARCOS.r
@@ -0,0 +1,3 @@
+library("devtools")
+
+devtools::install_github("dmattek/ARCOS")
---
+++ install_r_packages.r
@@ -0,0 +1,5 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+repos <- "https://cran.rstudio.com"
+
+install.packages(args, repos = repos)
