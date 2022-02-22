45e254452dbc082159f0d8b4386c567cf0c02cb8
Benjamin Graedel <benjamin.graedel@unibe.ch>
2022-02-22 14:38:37 +0100

    changed sample_data location

--- src/arcos_gui/_sample_data.py
+++ src/arcos_gui/_sample_data.py
@@ -9,10 +9,16 @@ from pathlib import Path
 
 from arcos_gui._arcos_widgets import stored_variables
 
+import os
+
+def resolve(name, basepath=None):
+    if not basepath:
+      basepath = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(basepath, name)
 
 def load_sample_data():
     """Load sample data into stored_variables"""
     stored_variables.filename_for_sample_data = str(
-        Path("arcos_gui/sample_data/arcos_data.csv")
+        resolve(Path("sample_data/arcos_data.csv"))
     )
     return []
