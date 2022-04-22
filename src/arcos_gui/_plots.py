46b3cd03f1c801c1666d82fa740d1af7ddc50134
Benjamin Graedel <benjamin.graedel@unibe.ch>
2022-04-22 17:09:47 +0200

    update collevent layer

--- src/arcos_gui/_plots.py
+++ src/arcos_gui/_plots.py
@@ -304,7 +304,7 @@ class NoodlePlot(QtWidgets.QWidget):
             array_seq_colids,
             np.unique(array_seq_colids[:, :2], axis=0, return_index=True)[1][1:],
         )
-        colors = plt.cm.viridis_r(
+        colors = plt.cm.tab20(
             np.linspace(0, 1, np.unique(array_seq_colids[:, -1]).shape[0])
         )
         return grouped_array, colors
@@ -342,13 +342,18 @@ class NoodlePlot(QtWidgets.QWidget):
             self.ax.yaxis.label.set_color("white")
             self.ax.tick_params(colors="white", which="both")
             self.ax.axis("on")
-            for dat in self.dat_grpd:
-                self.ax.plot(
-                    dat[:, 2], dat[:, 4], c=self.colors[int(dat[0, -1])], picker=True
-                )
             self.ax.set_xlabel("Time Point")
             self.ax.set_ylabel("Position")
-            self.fig.canvas.draw_idle()
+            for dat in self.dat_grpd:
+                self.ax.plot(
+                    dat[:, 2],
+                    dat[:, 4],
+                    c=self.colors[int(dat[0, -1])],
+                    picker=True,
+                    pickradius=20,
+                )
+
+            # self.fig.canvas.draw_idle()
             self.nbr_collev = self.stats.shape[0]
             self.annot = self.ax.annotate(
                 "",
@@ -358,8 +363,10 @@ class NoodlePlot(QtWidgets.QWidget):
                 fontsize=7,
                 color="white",
                 clip_on=True,
+                animated=True,
             )
             self.annot.set_visible(False)
+            self.bm = BlitManager(self.canvas, [self.annot])
 
     def update_annot(self, ind, line):
         x, y = line.get_data()
@@ -399,11 +406,11 @@ class NoodlePlot(QtWidgets.QWidget):
                 if cont:
                     self.update_annot(ind, line)
                     self.annot.set_visible(True)
-                    self.fig.canvas.draw_idle()
+                    self.bm.update()
                 else:
                     if vis:
                         self.annot.set_visible(False)
-                        self.fig.canvas.draw_idle()
+                        self.bm.update()
 
     # def on_pick(self, event):
     #     ind = event.ind
@@ -457,6 +464,79 @@ class NoodlePlot(QtWidgets.QWidget):
 
     def register_callback_on_collid_pick(self, callback):
         self._callbacks.append(callback)
+
+
+class BlitManager:
+    def __init__(self, canvas, animated_artists=()):
+        """
+        Parameters
+        ----------
+        canvas : FigureCanvasAgg
+            The canvas to work with, this only works for sub-classes of the Agg
+            canvas which have the `~FigureCanvasAgg.copy_from_bbox` and
+            `~FigureCanvasAgg.restore_region` methods.
+
+        animated_artists : Iterable[Artist]
+            List of the artists to manage
+        """
+        self.canvas = canvas
+        self._bg = None
+        self._artists = []
+
+        for a in animated_artists:
+            self.add_artist(a)
+        # grab the background on every draw
+        self.cid = canvas.mpl_connect("draw_event", self.on_draw)
+
+    def on_draw(self, event):
+        """Callback to register with 'draw_event'."""
+        cv = self.canvas
+        if event is not None:
+            if event.canvas != cv:
+                raise RuntimeError
+        self._bg = cv.copy_from_bbox(cv.figure.bbox)
+        self._draw_animated()
+
+    def add_artist(self, art):
+        """
+        Add an artist to be managed.
+
+        Parameters
+        ----------
+        art : Artist
+
+            The artist to be added.  Will be set to 'animated' (just
+            to be safe).  *art* must be in the figure associated with
+            the canvas this class is managing.
+
+        """
+        if art.figure != self.canvas.figure:
+            raise RuntimeError
+        art.set_animated(True)
+        self._artists.append(art)
+
+    def _draw_animated(self):
+        """Draw all of the animated artists."""
+        fig = self.canvas.figure
+        for a in self._artists:
+            fig.draw_artist(a)
+
+    def update(self):
+        """Update the screen with animated artists."""
+        cv = self.canvas
+        fig = cv.figure
+        # paranoia in case we missed the draw event,
+        if self._bg is None:
+            self.on_draw(None)
+        else:
+            # restore the background
+            cv.restore_region(self._bg)
+            # draw all of the animated artists
+            self._draw_animated()
+            # update the GUI state
+            cv.blit(fig.bbox)
+        # let the GUI event loop process anything it has to do
+        cv.flush_events()
 
 
 class TimeSeriesPlots(QtWidgets.QWidget):
